38c1ff3079542f9879f7e9b37adc70c88816c453
Marcin Matuszkiewicz <marcin201@gmail.com>
2017-10-28 22:43:38 -0700

    added saveDOD command

--- calendar/dodcal.php
+++ calendar/dodcal.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set('display_errors', 1);
-
+ 
 require_once('config.php');
 
 define('EC_STATUS', 'status');
@@ -12,6 +12,7 @@ define('EC_STATUS_SUCCESS', 'success');
 define('CMD_GET_SHIFTS', 'getShifts');
 define('CMD_GET_LOCATIONS', 'getLocations');
 define('CMD_GET_DODS', 'getDODs');
+define('CMD_SAVE_DOD', 'saveDOD');
 define('CMD_SAVE_COVERAGE', 'saveCoverage');
 define('CMD_GET_COVERAGE', 'getCoverage');
 
@@ -28,6 +29,7 @@ define('DB_COL_DOD_ID', 'dod_id');
 define('DB_COL_DOD_NAME', 'dod_name');
 define('DB_COL_DOD_EMAIL', 'dod_email');
 define('DB_COL_DOD_CELL', 'dod_cell');
+define('DB_COL_DOD_ACTIVE', 'dod_active');
 
 define('DB_TB_CALENDAR', 'calendar');
 define('DB_COL_DATE','date');
@@ -47,7 +49,30 @@ function queryGetLocations() {
 }
 
 function queryGetDODs() {
-  $query = 'SELECT '.DB_COL_DOD_ID.','.DB_COL_DOD_NAME.','.DB_COL_DOD_EMAIL.','.DB_COL_DOD_CELL. ' FROM '.DB_TB_DODS;
+  $query = 'SELECT '.DB_COL_DOD_ID.','.DB_COL_DOD_NAME.','.DB_COL_DOD_EMAIL.','.DB_COL_DOD_CELL.','.DB_COL_DOD_ACTIVE;
+  $query .=' FROM '.DB_TB_DODS;
+  return $query;
+}
+
+// This will generate a query which will generate insert / or update the dod
+function querySaveDOD($dod_id, $dod_name, $dod_email, $dod_cell, $dod_active) {
+  $query = "INSERT INTO ".DB_TB_DODS." (";
+  $query .= DB_COL_DOD_ID.", ";
+  $query .= DB_COL_DOD_NAME.", ";
+  $query .= DB_COL_DOD_EMAIL.", ";
+  $query .= DB_COL_DOD_CELL.", ";
+  $query .= DB_COL_DOD_ACTIVE.") ";
+  $query .= "VALUES (";
+  $query .= $dod_id ? $dod_id.", ": "NULL".", ";
+  $query .= "'".$dod_name."', ";
+  $query .= "'".$dod_email."', ";
+  $query .= "'".$dod_cell."', ";
+  $query .= $dod_active.") ";
+  $query .= "ON DUPLICATE KEY UPDATE ";
+  $query .= DB_COL_DOD_NAME."='".$dod_name."', ";
+  $query .= DB_COL_DOD_EMAIL."='".$dod_email."', ";
+  $query .= DB_COL_DOD_CELL."='".$dod_cell."', ";
+  $query .= DB_COL_DOD_ACTIVE."=".$dod_active;
   return $query;
 }
 
@@ -87,9 +112,7 @@ function queryUpdateCoverage($date, $shift_id, $location_id, $dod_id, $dod_next,
   return $query;
 }
 
-// echo quereyGetCoverage('2017-08-07', '1', '1');
-// echo queryInsertCoverage('2017-08-08', '1', '1', '3', '1', 'Steve (full)');
-// echo queryUpdateCoverage('2017-08-07', '1', '1', '10', '1', 'Kelly');
+// echo querySaveDOD(6, 'Marcin', 'marcin@jewelmirorr.com', '', 0);
 // exit();
 
 $link = mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE);
@@ -136,6 +159,16 @@ if (isset($_POST['cmd'])) {
     }
     $exitCode[EC_STATUS] = EC_STATUS_SUCCESS;
     $exitCode[EC_DATA] = json_encode($data);
+  } else if ($cmd == CMD_SAVE_DOD) {
+    $dod_id = $_POST['dod_id'];
+    $dod_name = $_POST['dod_name'];
+    $dod_email = $_POST['dod_email'];
+    $dod_cell = $_POST['dod_cell'];
+    $dod_active = $_POST['dod_active'];
+    $query = querySaveDOD($dod_id, $dod_name, $dod_email, $dod_cell, $dod_active);
+    mysqli_query($link, $query);
+    $exitCode[EC_STATUS] = EC_STATUS_SUCCESS;
+    $exitCode[EC_DATA] = $cmd.": OK";      
   } else if ($cmd == CMD_SAVE_COVERAGE) {
     $date = $_POST['date'];
     $shift_id = $_POST['shift_id'];
